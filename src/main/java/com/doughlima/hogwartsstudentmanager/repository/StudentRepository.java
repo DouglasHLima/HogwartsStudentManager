38f0dd85d3142eb2fd9d7d2c4673484647baa50b
DouglasHLima <douglashdelima@gmail.com>
2022-04-20 22:30:13 -0300

    feat: repository

---
+++ src/main/java/com/doughlima/hogwartsstudentmanager/repository/StudentRepository.java
@@ -0,0 +1,11 @@
+package com.doughlima.hogwartsstudentmanager.repository;
+
+import com.doughlima.hogwartsstudentmanager.domain.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+}
